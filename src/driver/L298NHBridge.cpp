577f07716274374489025a9cf599a89fdbec1ee4
jwolf02 <jwolf02@students.uni-mainz.de>
2019-11-06 19:45:15 +0100

    added motor test

--- src/driver/L298NHBridge.cpp
+++ src/driver/L298NHBridge.cpp
@@ -22,6 +22,7 @@ L298NHBridge::L298NHBridge(int ENA, int IN1, int IN2, int IN3, int IN4, int ENB,
     }
 
     #ifdef RASPBERRY_PI
+    wiringPiSetupGpio();
     pinMode(ENA, OUTPUT);
     pinMode(IN1, OUTPUT);
     pinMode(IN2, OUTPUT);
