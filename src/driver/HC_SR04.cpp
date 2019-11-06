399517baed791acddf5ab54d37f92b58665500b4
jwolf02 <jwolf02@students.uni-mainz.de>
2019-11-06 19:36:58 +0100

    added motor test

--- src/driver/HC_SR04.cpp
+++ src/driver/HC_SR04.cpp
@@ -1,9 +1,13 @@
 #include <HC_SR04.hpp>
-#include <wiringPi.h>
 #include <unistd.h>
 #include <ctime>
 
+#ifdef RASPBERRY_PI
+#include <wiringPi.h>
+#endif
+
 HC_SR04::HC_SR04(int trigger, int echo) {
+    #ifdef RASPBERRY_PI
     wiringPiSetupGpio();
     _trigger = trigger;
     _echo = echo;
@@ -11,13 +15,17 @@ HC_SR04::HC_SR04(int trigger, int echo) {
     pinMode(echo, INPUT);
     digitalWrite(trigger, LOW);
     sleep(2);
+    #endif
 }
 
 HC_SR04::~HC_SR04() {
+    #ifdef RASPBERRY_PI
     digitalWrite(_trigger, LOW);
+    #endif
 }
 
 double HC_SR04::distance() {
+    #ifdef RASPBERRY_PI
     digitalWrite(_trigger, HIGH);
     usleep(10);
     digitalWrite(_trigger, LOW);
@@ -34,4 +42,7 @@ double HC_SR04::distance() {
 
     const double time_of_flight = double(pulse_end_time - pulse_start_time) / CLOCKS_PER_SEC;
     return time_of_flight * 17250.0;
+    #else
+    return 0.0;
+    #endif
 }
